57045aa7999377f896967c07029eb7161ff107ed
Waqas Ali <sawaldherwa@gmail.com>
2024-11-08 18:30:56 +0500

    add some suspense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,32 @@
+import {MobileNav} from '@/components/mobile-nav';
+import {TopBar} from '@/components/top-bar';
+import type {Metadata} from 'next';
+import {Inter} from 'next/font/google';
+import {Suspense} from 'react';
 import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import { MobileNav } from '@/components/mobile-nav';
-import { TopBar } from '@/components/top-bar';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({subsets: ['latin']});
 
 export const metadata: Metadata = {
   title: 'Xpad Game',
   description: 'Ultimate Fighter Championship on Xpad',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>
-        <TopBar />
-        <div className="pt-16"> {/* Spacer for top bar */}
-          {children}
-        </div>
-        <MobileNav />
-        <div className="h-16 md:h-0" /> {/* Spacer for mobile nav */}
-      </body>
+      <Suspense>
+        <body className={inter.className}>
+          <TopBar />
+          <div className="pt-16">
+            {' '}
+            {/* Spacer for top bar */}
+            {children}
+          </div>
+          <MobileNav />
+          <div className="h-16 md:h-0" /> {/* Spacer for mobile nav */}
+        </body>
+      </Suspense>
     </html>
   );
 }
