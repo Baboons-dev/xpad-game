05f0c0fc4f0f8e3a8d2593cf563f5b7908f67c7d
hasibbaboon <hasib@baboons.nl>
2024-11-11 17:24:45 +0600

    change bot key

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const web_link = "https://xplay.baboons.tech/";
 const TOKEN = "8169861892:AAHmobMRPylA7SLW9Z-kgOS7BQNZOQq6JYw";
 
 const bot = new Telegraf(TOKEN);
-var secretKey = "02PhgD8F4bgtA0T8WB2/y0dvrtHKXXMwtht3HZgyHSzAUrJwXRYjUV9z0lLB3sNu";
+var secretKey = "LefjQ2pEXmiy/nNZvEJ43i8hJuaAnzbA1Cbn1hOuAgA=";
 
 function random16String() {
   let result = "";
@@ -15,7 +15,7 @@ function random16String() {
 }
 
 function encrypt(text) {
-  var derived_key = crypto.enc.Base64.parse(secretKey);
+  const derived_key = crypto.enc.Base64.parse(secretKey);
 
   let randomInt16 = random16String();
   // Generating a random 16-digit string to be used as the initialization vector (IV)
@@ -26,21 +26,21 @@ function encrypt(text) {
     iv: iv,
     mode: crypto.mode.CBC,
   }).toString();
-
-  // Combining the random 16-digit number and the encrypted payload
-  return randomInt16 + payload;
+ if (payload.includes('+')) {
+    encrypt(text);
+  } else {
+    return randomInt16 + payload;
+  }
 }
 
 bot.command("start", async (ctx) => {
   const userId = ctx.update.message.from.id;
   var encrypted = encrypt(userId.toString());
-
-  let web_final_link= web_link + "?tgId=" + encodeURIComponent(encrypted);
   ctx.setChatMenuButton({
     text: "Game",
     type: "web_app",
     web_app: {
-      url: web_final_link,
+      url: web_link + "?tgId=" + encodeURIComponent(encrypted),
     },
   });
   // sets Web App as the menu button for current chat
