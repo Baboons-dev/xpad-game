d823f6dc90990c07dfae176f77dc09d63d66c987
hasibbaboon <hasib@baboons.nl>
2024-11-12 09:25:10 +0600

    test

--- src/app/authenticate/page.tsx
+++ src/app/authenticate/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, {useEffect, useState} from "react";
 import {useRouter, useSearchParams} from "next/navigation";
-import {Button, Spin} from "antd";
+import {Button, message, Spin} from "antd";
 import {useUser} from "@/hooks";
 import {twitterLogin} from "@/api/apiCalls/user";
 import {useTelegram} from "@/providers/TelegramProvider";
@@ -14,7 +14,7 @@ export default function Authenticate() {
     const tgId = searchParams.get("tgId");
     const code = searchParams.get("code");
     const tId = searchParams.get("tId");
-    // const codeVerifier = searchParams.get("codeVerifier");
+    const codeVerifier = searchParams.get("codeVerifier");
     const [twUrl, setTwUrl] = useState('')
     const {loginTwitter, getCurrentUser} = useUser();
     const user = useStore((state) => state.user);
@@ -22,21 +22,30 @@ export default function Authenticate() {
     const setAccessToken = useSelector.use.setAccessToken();
     const setRefreshToken = useSelector.use.setRefreshToken();
     useEffect(() => {
-        if (state && code && tgId && tId ) {
-            loginTwitter({state: state, code: code, tId: tId, tgId: tgId})
+        if (state && code && tgId && tId && codeVerifier) {
+            loginTwitter({state: state, code: code, tId: tId, tgId: tgId,codeVerifier:codeVerifier}).then(()=>{
+                message.success('Login Success')
+                setTwUrl('')
+            })
         }
-    }, [tId, code, tgId, state]);
+    }, [tId, code, tgId, state,codeVerifier]);
     const {telegram_user} = useTelegram();
     const login = async () => {
         if (telegram_user && tgId)
             try {
                 const response = await twitterLogin(tgId as string, telegram_user?.id.toString() as string)
                 console.log(response.data)
-                if (response?.url) {
-                    const queryParams = new URL(response?.url);
-                    queryParams.searchParams.set("redirect_uri", "https://xplay.baboons.tech/authenticate?tgId=" + encodeURI(tgId) + '&tId=' + encodeURI(telegram_user?.id.toString() as string));
-                    setTwUrl(queryParams.toString())
+                if (response?.url && response?.code_verifier) {
+                     const queryParams = new URL(response?.url);
+                    queryParams.searchParams.set("redirect_uri", "https://xplay.baboons.tech/authenticate?codeVerifier="+encodeURI(response?.code_verifier)+"&tgId=" + encodeURI(tgId) + '&tId=' + encodeURI(telegram_user?.id.toString() as string));
+                    setTwUrl(response?.url)
+
+
+                    // const queryParams = new URL(response?.url);
+                    // queryParams.searchParams.set("redirect_uri", "https://xplay.baboons.tech/authenticate?tgId=" + encodeURI(tgId) + '&tId=' + encodeURI(telegram_user?.id.toString() as string));
+                    // setTwUrl(response?.url)
                 } else if (response.data.access) {
+                    message.success('Login Success')
                     localStorage.setItem('token', response.data.access);
                     localStorage.setItem('refreshToken', response.data.refresh);
                     setAccessToken(response.data.access as string);
@@ -44,10 +53,12 @@ export default function Authenticate() {
                     setTimeout(async () => {
                         await getCurrentUser();
                     }, 500)
-                    return true;
+
+                    setTwUrl('')
                 }
 
             } catch (e) {
+                message.error('Code error')
                 console.log(e)
             }
 
@@ -59,7 +70,7 @@ export default function Authenticate() {
         }
     }, [user, accessToken]);
     return (<div className={'flex justify-center'}>
-            <Button onClick={() => login()}>Login with twitter</Button>
+        {state && code ?<Spin/>:<Button onClick={() => login()}>Login with twitter</Button>}
             {twUrl && <div>
                 <Button onClick={async () => {
                     await navigator.clipboard.writeText(twUrl);
