a815d4370ce70fc9130a0c092155648a551b3b8a
neelamwaqar123 <neelamwaqar37@gmail.com>
2024-11-13 15:53:48 +0500

    set chakra ui

--- src/app/authenticate/page.tsx
+++ src/app/authenticate/page.tsx
@@ -1,85 +1,112 @@
-'use client'
-import React, {useEffect, useState} from "react";
-import {useRouter, useSearchParams} from "next/navigation";
-import {Button, message, Spin} from "antd";
-import {useUser} from "@/hooks";
-import {twitterLogin} from "@/api/apiCalls/user";
-import {useTelegram} from "@/providers/TelegramProvider";
-import {useSelector, useStore} from "@/store";
+"use client";
+import React, { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Button, message, Spin } from "antd";
+import { useUser } from "@/hooks";
+import { twitterLogin } from "@/api/apiCalls/user";
+import { useTelegram } from "@/providers/TelegramProvider";
+import { useSelector, useStore } from "@/store";
 
 export default function Authenticate() {
-    const router = useRouter();
-    const searchParams = useSearchParams();
-    const state = searchParams.get("state");
-    const tgId = searchParams.get("tgId");
-    const code = searchParams.get("code");
-    const tId = searchParams.get("tId");
-    const codeVerifier = searchParams.get("codeVerifier");
-    const [twUrl, setTwUrl] = useState('')
-    const {loginTwitter, getCurrentUser} = useUser();
-    const user = useStore((state) => state.user);
-    const accessToken = useStore((state) => state.accessToken);
-    const setAccessToken = useSelector.use.setAccessToken();
-    const setRefreshToken = useSelector.use.setRefreshToken();
-    useEffect(() => {
-        if (state && code && tgId && tId && codeVerifier) {
-            loginTwitter({state: state, code: code, tId: tId, tgId: tgId,codeVerifier:codeVerifier})
-        }
-    }, [tId, code, tgId, state,codeVerifier]);
-    const {telegram_user} = useTelegram();
-    const login = async () => {
-        if (telegram_user && tgId)
-            try {
-                const response = await twitterLogin(tgId as string, telegram_user?.id.toString() as string)
-                console.log(response.data)
-                if (response?.url && response?.code_verifier) {
-                     const queryParams = new URL(response?.url);
-                    queryParams.searchParams.set("redirect_uri", "https://xplay.baboons.tech/authenticate?codeVerifier="+encodeURI(response?.code_verifier)+"&tgId=" + encodeURI(tgId) + '&tId=' + encodeURI(telegram_user?.id.toString() as string));
-                    setTwUrl(queryParams.toString())
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const state = searchParams.get("state");
+  const tgId = searchParams.get("tgId");
+  const code = searchParams.get("code");
+  const tId = searchParams.get("tId");
+  const codeVerifier = searchParams.get("codeVerifier");
+  const [twUrl, setTwUrl] = useState("");
+  const { loginTwitter, getCurrentUser } = useUser();
+  const user = useStore((state) => state.user);
+  const accessToken = useStore((state) => state.accessToken);
+  const setAccessToken = useSelector.use.setAccessToken();
+  const setRefreshToken = useSelector.use.setRefreshToken();
 
-                    // const queryParams = new URL(response?.url);
-                    // queryParams.searchParams.set("redirect_uri", "https://xplay.baboons.tech/authenticate?tgId=" + encodeURI(tgId) + '&tId=' + encodeURI(telegram_user?.id.toString() as string));
-                    // setTwUrl(response?.url)
-                } else if (response.data.access) {
-                    message.success('Login Success')
-                    localStorage.setItem('token', response.data.access);
-                    localStorage.setItem('refreshToken', response.data.refresh);
-                    setAccessToken(response.data.access as string);
-                    setRefreshToken(response.data.refresh as string);
-                    setTimeout(async () => {
-                        await getCurrentUser();
-                    }, 500)
-
-                    setTwUrl('')
-                }
-
-            } catch (e) {
-                message.error('Code error')
-                console.log(e)
-            }
-
+  useEffect(() => {
+    if (state && code && tgId && tId && codeVerifier) {
+      loginTwitter({
+        state: state,
+        code: code,
+        tId: tId,
+        tgId: tgId,
+        codeVerifier: codeVerifier,
+      });
     }
-    useEffect(() => {
-        if (accessToken && user) {
+  }, [tId, code, tgId, state, codeVerifier]);
+  const { telegram_user } = useTelegram();
+  const login = async () => {
+    if (telegram_user && tgId)
+      try {
+        const response = await twitterLogin(
+          tgId as string,
+          telegram_user?.id.toString() as string,
+        );
+        console.log(response.data);
+        if (response?.url && response?.code_verifier) {
+          const queryParams = new URL(response?.url);
+          queryParams.searchParams.set(
+            "redirect_uri",
+            "https://xplay.baboons.tech/authenticate?codeVerifier=" +
+              encodeURI(response?.code_verifier) +
+              "&tgId=" +
+              encodeURI(tgId) +
+              "&tId=" +
+              encodeURI(telegram_user?.id.toString() as string),
+          );
+          setTwUrl(queryParams.toString());
 
-            router.push('/?tgId=' + tgId)
+          // const queryParams = new URL(response?.url);
+          // queryParams.searchParams.set("redirect_uri", "https://xplay.baboons.tech/authenticate?tgId=" + encodeURI(tgId) + '&tId=' + encodeURI(telegram_user?.id.toString() as string));
+          // setTwUrl(response?.url)
+        } else if (response.data.access) {
+          message.success("Login Success");
+          localStorage.setItem("token", response.data.access);
+          localStorage.setItem("refreshToken", response.data.refresh);
+          setAccessToken(response.data.access as string);
+          setRefreshToken(response.data.refresh as string);
+          setTimeout(async () => {
+            await getCurrentUser();
+          }, 500);
+
+          setTwUrl("");
         }
-    }, [user, accessToken]);
-    return (<div className={'flex justify-center'}>
-        {state && code ?<Spin/>:<Button onClick={() => login()}>Login with twitter</Button>}
-            {twUrl && <div>
-                <Button onClick={async () => {
-                    await navigator.clipboard.writeText(twUrl);
-                    alert('copied')
-                }}>Copy Link</Button>
-                <Button onClick={() => {
-                    const newWindow = window.open(twUrl as string, '_blank');
-                    if (newWindow) newWindow.opener = null;
-                }}>
-                    Authenicate Link
-                </Button>
-            </div>
-            }
+      } catch (e) {
+        message.error("Code error");
+        console.log(e);
+      }
+  };
+  useEffect(() => {
+    if (accessToken && user) {
+      router.push("/?tgId=" + tgId);
+    }
+  }, [user, accessToken]);
+  return (
+    <div className={"flex justify-center"}>
+      {state && code ? (
+        <Spin />
+      ) : (
+        <Button onClick={() => login()}>Login with twitter</Button>
+      )}
+      {twUrl && (
+        <div>
+          <Button
+            onClick={async () => {
+              await navigator.clipboard.writeText(twUrl);
+              alert("copied");
+            }}
+          >
+            Copy Link
+          </Button>
+          <Button
+            onClick={() => {
+              const newWindow = window.open(twUrl as string, "_blank");
+              if (newWindow) newWindow.opener = null;
+            }}
+          >
+            Authenicate Link
+          </Button>
         </div>
-    );
+      )}
+    </div>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,61 @@
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+// import { Suspense } from "react";
+// import "./globals.css";
+// import { TelegramProvider } from "@/providers/TelegramProvider";
+// import { AppProvider } from "@/providers/AppProvider";
+// import Script from "next/script";
+// import { TopBar } from "@/components/top-bar";
+// import { MobileNav } from "@/components/mobile-nav";
+// import { Providers } from "./provider";
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata: Metadata = {
+//   title: "Xpad Game",
+//   description: "Ultimate Fighter Championship on Xpad",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en" className="dark">
+//       <head>
+//         <Script
+//           src="https://telegram.org/js/telegram-web-app.js"
+//           strategy="beforeInteractive"
+//         />
+//         <meta
+//           content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, viewport-fit=cover"
+//           name="viewport"
+//         />
+//       </head>
+//       <body>
+//         <Suspense>
+//           <TelegramProvider>
+//             <AppProvider>{children}</AppProvider>
+//             <div className="h-16 md:h-0" /> {/* Spacer for mobile nav */}
+//           </TelegramProvider>
+//         </Suspense>
+//       </body>
+//     </html>
+//   );
+// }
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Suspense } from "react";
 import "./globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 import { TelegramProvider } from "@/providers/TelegramProvider";
 import { AppProvider } from "@/providers/AppProvider";
 import Script from "next/script";
 import { TopBar } from "@/components/top-bar";
 import { MobileNav } from "@/components/mobile-nav";
+import { Providers } from "./provider";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,7 +69,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className={`${inter.className} dark`}>
       <head>
         <Script
           src="https://telegram.org/js/telegram-web-app.js"
@@ -31,16 +80,22 @@ export default function RootLayout({
           name="viewport"
         />
       </head>
-      <Suspense>
-        <TelegramProvider>
-          <body className={inter.className}>
-            {/* <TopBar /> */}
-            {/* <div>{children}</div> */}
-            <AppProvider>{children}</AppProvider>
-            <div className="h-16 md:h-0" /> {/* Spacer for mobile nav */}
-          </body>
-        </TelegramProvider>
-      </Suspense>
+      <body>
+        <ChakraProvider>
+          <Providers>
+            <Suspense>
+              <TelegramProvider>
+                <AppProvider>
+                  <TopBar />
+                  {children}
+                  <MobileNav />
+                  <div className="h-16 md:h-0" /> {/* Spacer for mobile nav */}
+                </AppProvider>
+              </TelegramProvider>
+            </Suspense>
+          </Providers>
+        </ChakraProvider>
+      </body>
     </html>
   );
 }
