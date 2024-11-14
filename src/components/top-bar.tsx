721d5e59b97e36de94b6534297108d5d2862dcc2
Tom <tom@baboonbranding.nl>
2024-11-14 11:00:24 +0100

    set back neelams user thing before she will get mad at me

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -7,7 +7,7 @@ import XpIcon from "@/icons/XpIcon";
 
 export function TopBar() {
   const { logout } = useUser();
-
+  const user = useStore((state) => state.user);
   return (
     <Box
       display="flex"
@@ -16,38 +16,64 @@ export function TopBar() {
       padding="12px 16px"
     >
       <Box display="flex" gap="13px" flexDirection="row">
-        <Box
-          padding={"1px"}
-          height="40px"
-          width="40px"
-          zIndex={2}
-          style={{
-            borderRadius: "8px",
-            backgroundImage:
-              "linear-gradient(#1F1F1F, #1F1F1F), linear-gradient(#1ED1FC, #47E473, #3AFF65)",
-            backgroundOrigin: "border-box",
-            backgroundClip: "content-box, border-box",
-          }}
-        >
-          <Box height="40px" width="40px" overflow="hidden">
-            <Image
-              src={ProfilePicture.src}
-              objectFit="cover"
-              height="100%"
-              width="100%"
-              style={{ borderRadius: "0" }}
-            />
+        {false ? (
+          <Box
+            padding={"1px"}
+            height="40px"
+            width="40px"
+            zIndex={2}
+            style={{
+              borderRadius: "8px",
+              backgroundImage:
+                "linear-gradient(#1F1F1F, #1F1F1F), linear-gradient(#1ED1FC, #47E473, #3AFF65)",
+              backgroundOrigin: "border-box",
+              backgroundClip: "content-box, border-box",
+            }}
+          >
+            <Box height="40px" width="40px" overflow="hidden">
+              <Image
+                src={user?.avatar}
+                objectFit="cover" // Ensures the image fills the square box
+                height="100%"
+                width="100%"
+                style={{ borderRadius: "0" }} // Removes any circular styling
+              />
+            </Box>
           </Box>
-        </Box>
+        ) : (
+          <Box
+            padding={"1px"}
+            height="40px"
+            width="40px"
+            zIndex={2}
+            style={{
+              borderRadius: "8px",
+              backgroundImage:
+                "linear-gradient(#1F1F1F, #1F1F1F), linear-gradient(#1ED1FC, #47E473, #3AFF65)",
+              backgroundOrigin: "border-box",
+              backgroundClip: "content-box, border-box",
+            }}
+          >
+            <Box height="40px" width="40px" overflow="hidden">
+              <Image
+                src={ProfilePicture.src}
+                objectFit="cover" // Ensures the image fills the square box
+                height="100%"
+                width="100%"
+                style={{ borderRadius: "0" }} // Removes any circular styling
+              />
+            </Box>
+          </Box>
+        )}
         <Box display="flex" justifyContent="center" alignItems="center">
           <Text
             color="#FFF"
             fontSize="16px"
             fontStyle="normal"
             fontWeight="800"
-            lineHeight="normal"
+            lineHeight=" normal"
           >
-            Demo User
+            Bart Ermens
           </Text>
         </Box>
       </Box>
@@ -57,13 +83,15 @@ export function TopBar() {
         </Box>
         <Box display="flex" justifyContent="center" alignItems="center">
           <Text
-            color="#33DBB8"
+            color=" #33DBB8"
             fontSize="16px"
             fontStyle="normal"
             fontWeight="600"
-            lineHeight="normal"
+            lineHeight=" normal"
           >
+            {" "}
             2500
+            {/* {user?.points.toLocaleString()} */}
           </Text>
         </Box>
       </Box>
