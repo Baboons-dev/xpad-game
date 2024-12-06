7a78e00e70a1922bc10a4f3b1194625da6c816b0
Waqas Ali <sawaldherwa@gmail.com>
2024-12-06 15:02:52 +0500

    add layerX chain

--- src/providers/WalletProvider.tsx
+++ src/providers/WalletProvider.tsx
@@ -1,48 +1,37 @@
-"use client";
+'use client';
 
-import { projectId, wagmiAdapter } from "@/utils/wallet-configs";
-import { sepolia } from "@reown/appkit/networks";
-import { createAppKit } from "@reown/appkit/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { type ReactNode } from "react";
-import { cookieToInitialState, WagmiProvider, type Config } from "wagmi";
+import { defaultNetwork, networks, projectId, wagmiAdapter } from '@/utils/wallet-configs';
+import { createAppKit } from '@reown/appkit/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { type ReactNode } from 'react';
+import { cookieToInitialState, WagmiProvider, type Config } from 'wagmi';
 
 // Set up queryClient
 const queryClient = new QueryClient();
 
 if (!projectId) {
-  throw new Error("Project ID is not defined");
+  throw new Error('Project ID is not defined');
 }
 
 // Set up metadata
 const metadata = {
-  name: "xpad-game",
-  description: "AppKit Example",
-  url: "https://xplay.baboons.tech", // origin must match your domain & subdomain
-  icons: ["https://xplay.baboons.tech/logo.png"],
+  name: 'xpad-game',
+  description: 'AppKit Example',
+  url: 'https://xplay.baboons.tech', // origin must match your domain & subdomain
+  icons: ['https://xplay.baboons.tech/logo.png'],
 };
 
 // Create the modal
 const modal = createAppKit({
   adapters: [wagmiAdapter],
   projectId,
-  // also change in utils/wallet-configs.ts
-  networks: [sepolia],
-  defaultNetwork: sepolia,
+  networks: networks as any,
+  defaultNetwork: defaultNetwork,
   metadata: metadata,
 });
 
-function WalletProvider({
-  children,
-  cookies,
-}: {
-  children: ReactNode;
-  cookies: string | null;
-}) {
-  const initialState = cookieToInitialState(
-    wagmiAdapter.wagmiConfig as Config,
-    cookies,
-  );
+function WalletProvider({ children, cookies }: { children: ReactNode; cookies: string | null }) {
+  const initialState = cookieToInitialState(wagmiAdapter.wagmiConfig as Config, cookies);
 
   // const isIOS = () => {
   //   // Refined iOS detection
@@ -69,10 +58,7 @@ function WalletProvider({
   // }, []);
 
   return (
-    <WagmiProvider
-      config={wagmiAdapter.wagmiConfig as Config}
-      initialState={initialState}
-    >
+    <WagmiProvider config={wagmiAdapter.wagmiConfig as Config} initialState={initialState}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </WagmiProvider>
   );
--- src/utils/wallet-configs.ts
+++ src/utils/wallet-configs.ts
@@ -1,16 +1,37 @@
-import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
-import { sepolia } from "@reown/appkit/networks";
-import { cookieStorage, createStorage } from "@wagmi/core";
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi';
+import { defineChain, sepolia } from '@reown/appkit/networks';
+import { cookieStorage, createStorage } from '@wagmi/core';
 
 // Get projectId from https://cloud.reown.com
-export const projectId = "812502bab4b58307bcf65fbb7f78356e";
+export const projectId = '812502bab4b58307bcf65fbb7f78356e';
 
 if (!projectId) {
-  throw new Error("Project ID is not defined");
+  throw new Error('Project ID is not defined');
 }
 
-// also change in providers/WalletProvider.tsx
-export const networks = [sepolia];
+export const layerXNetwork_test = defineChain({
+  id: 232232,
+  caipNetworkId: 'eip155:232232',
+  chainNamespace: 'eip155',
+  name: 'LayerX',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'LayerX',
+    symbol: 'LuX',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc-testnet.layerxscan.com'],
+    },
+  },
+  blockExplorers: {
+    default: { name: 'Explorer', url: 'https://testnet.layerxscan.com' },
+  },
+});
+
+export const defaultNetwork = layerXNetwork_test;
+
+export const networks = [layerXNetwork_test, sepolia];
 
 //Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
